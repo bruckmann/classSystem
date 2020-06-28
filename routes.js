cb597bdec042f684969a1a292228d84f0a88162c
iStrokes <felipebruckmann@hotmail.com.br>
2020-06-28 19:33:42 -0300

    back-end

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routes = express.Router()
+const fs = require('fs')
+const data = require('./data.json')
 module.exports = routes
 
 routes.get('/', (req, res) => {
@@ -7,16 +9,34 @@ routes.get('/', (req, res) => {
 })
 
 routes.get('/teachers', (req, res) => {
-    return res.render('teachers')
+    return res.render('teachers/index')
 })
 
 routes.get('/teachers/signin', (req, res) => {
-    return res.render('signin')
+    return res.render('teachers/signin')
 })
 
-routes.post('/teachers/signin', (req, res) => {
-    req.body
-    return res.send(req.body)
+routes.post('/teachers', (req, res) => {
+    
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") {
+        return res.send('EMPTY FIELDS')
+        }
+    }
+
+    data.teachers.push(req.body)
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+
+        if (err) {
+        return res.send('Write file error')
+        }
+
+        return res.redirect('/teachers')
+    })
+
 })
 
 routes.use(function(req, res) {
